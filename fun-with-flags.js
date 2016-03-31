e95f200ca9b8183ea34bee6603b23f408d6bba79
Fellman <fellmanj@pet-fellmanj-m.ds.ad.adp.com>
2016-03-31 13:46:12 -0700

    better fix for flags with no value

--- fun-with-flags.js
+++ fun-with-flags.js
@@ -50,18 +50,16 @@ function runFlags(scriptObj, args, flags, options) {
         // add the flags to the target
         _.each(obj.flags, function (flag, flagName) {
             var name = flag.name || flagName;
-            if (flags[flagName])
+
+            if (_.has(flags, flagName))
                 target[name] = flags[flagName];
-            else
-                target[name] = undefined;
         });
         // add the options to the target
         _.each(obj.options, function (option, optionName) {
             var name = option.name || optionName;
-            if (options[optionName])
+
+            if (_.has(options, optionName))
                 target[name] = options[optionName];
-            else
-                target[name] = undefined;
         });
         // run the command's exec function
         obj.exec.apply(obj, args)
