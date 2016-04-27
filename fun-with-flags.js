cb4323fa5cb0352e7e41a06cd67d15efcaefd9ea
Fellman <fellmanj@pet-fellmanj-m.ds.ad.adp.com>
2016-04-27 09:57:55 -0700

    indents multi-line descriptions

--- fun-with-flags.js
+++ fun-with-flags.js
@@ -123,10 +123,12 @@ function displayHelp(scriptObj, i) {
         if (typeof(val) === 'object' && val.description) {
             message += ': '+val.description;
             _.each(val.flags, function (flag, flagName) {
-                message += '\n'+spaces+'    -'+flagName+' : '+flag.description;
+                var description = flag.description.split('\n').join('\n'+spaces);
+                message += '\n'+spaces+'    -'+flagName+' : '+description;
             });
             _.each(val.options, function (option, optionName) {
-                message += '\n'+spaces+'    --'+optionName+' : '+option.description;
+                var description = option.description.split('\n').join('\n'+spaces);
+                message += '\n'+spaces+'    --'+optionName+' : '+description;
             });
             message+='\n';
 
