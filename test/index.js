ac0a6ee6ee03d2c648c527f0dbe2931e8df5556f
john fellman <john.fellman@gmail.com>
2017-08-19 13:06:54 -0700

    test(test): fix conflict in initial test

--- test/index.js
+++ test/index.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-var clWrap = require('../util/console_wrapper.js');
-// put some tests in here
-var result = clWrap(function () {
-  console.log('shit show');
-});
-
-console.log('shit show');
-=======
-const ass = require('assert')
+const assert = require('assert')
 
 describe('first test', function () {
   it('should return true', function () {
-    ass.equal(true, true)
+    assert.equal(true, true)
   })
 })
->>>>>>> 6330f95a1d9f2d98fad49d299535aa9f87476312
